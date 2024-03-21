d829e148b66ba9da30fe85188e7c4d8df709553b
chenyu <328800436@qq.com>
2024-03-21 14:48:38 +0800

    整理代码结构目录名称

---
+++ 第4章/InitDirect3D.cpp
@@ -0,0 +1,19 @@
+﻿//引入当前解决方案中的公共库文件，如果是在当前库的上一层级，则使用../，上两个层级使用../../依次类推 
+#include "../Common/d3dApp.h"
+#include <DirectXColors.h>
+using namespace DirectX;
+
+class InitDirect3DApp :public D3DApp
+{
+public:
+	InitDirect3DApp(HINSTANCE hInstance);
+	~InitDirect3DApp();
+};
+
+InitDirect3DApp::InitDirect3DApp(HINSTANCE hInstance):D3DApp(hInstance)
+{
+}
+
+InitDirect3DApp::~InitDirect3DApp()
+{
+}
