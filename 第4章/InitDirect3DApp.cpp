b023516f8f5e78357927cc57f9aafbc53737d7f3
staly_xiaoyu <54066197+yunyufeiwei@users.noreply.github.com>
2025-06-04 23:11:40 +0800

    添加注释

--- 第4章/InitDirect3DApp.cpp
+++ 第4章/InitDirect3DApp.cpp
@@ -1,12 +1,4 @@
-﻿//***************************************************************************************
-// Init Direct3D.cpp by Frank Luna (C) 2015 All Rights Reserved.
-//
-// Demonstrates the sample framework by initializing Direct3D, clearing 
-// the screen, and displaying frame stats.
-//
-//***************************************************************************************
-
-#include "../Common/d3dApp.h"
+﻿#include "../Common/d3dApp.h"
 #include <DirectXColors.h>
 
 using namespace DirectX;
@@ -78,46 +70,42 @@ void InitDirect3DApp::Update(const GameTimer& gt)
 
 void InitDirect3DApp::Draw(const GameTimer& gt)
 {
-    // Reuse the memory associated with command recording.
-    // We can only reset when the associated command lists have finished execution on the GPU.
+    // 重复使用记录命令的相关内存
+    // 只有当与GPU关联的命令列表执行完成时，我们才能将其重置
 	ThrowIfFailed(mDirectCmdListAlloc->Reset());
 
-	// A command list can be reset after it has been added to the command queue via ExecuteCommandList.
-    // Reusing the command list reuses memory.
+	// 通过ExecuteCommandList方法将某个命令列表加入队列后，我们便可以重置该命令列表。以此来复用命令列表及其内存
     ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), nullptr));
 
-	// Indicate a state transition on the resource usage.
-	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(),
-		D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET));
+	// 对资源的状态进行转换，将资源从呈现状态转换为渲染目标状态
+	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET));
 
-    // Set the viewport and scissor rect.  This needs to be reset whenever the command list is reset.
+    // 设置视口和裁剪矩形。他们需要随着命令列表的重置而重置
     mCommandList->RSSetViewports(1, &mScreenViewport);
     mCommandList->RSSetScissorRects(1, &mScissorRect);
 
-    // Clear the back buffer and depth buffer.
+    // 清除后台缓冲区和深度缓冲区
 	mCommandList->ClearRenderTargetView(CurrentBackBufferView(), Colors::LightSteelBlue, 0, nullptr);
 	mCommandList->ClearDepthStencilView(DepthStencilView(), D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.0f, 0, 0, nullptr);
 	
-    // Specify the buffers we are going to render to.
+    // 制定将要渲染的缓冲区
 	mCommandList->OMSetRenderTargets(1, &CurrentBackBufferView(), true, &DepthStencilView());
 	
-    // Indicate a state transition on the resource usage.
+    // 再次对资源状态进行转换，将资源从渲染目标转换为呈现状态
 	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(),
 		D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
 
-    // Done recording commands.
+    // 完成命令的记录
 	ThrowIfFailed(mCommandList->Close());
  
-    // Add the command list to the queue for execution.
+    // 将待执行的命令列表加入命令队列
 	ID3D12CommandList* cmdsLists[] = { mCommandList.Get() };
 	mCommandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
 	
-	// swap the back and front buffers
+	// 交换后台缓冲区和前台缓冲区
 	ThrowIfFailed(mSwapChain->Present(0, 0));
 	mCurrBackBuffer = (mCurrBackBuffer + 1) % SwapChainBufferCount;
 
-	// Wait until frame commands are complete.  This waiting is inefficient and is
-	// done for simplicity.  Later we will show how to organize our rendering code
-	// so we do not have to wait per frame.
+	// 等待此帧的命令执行完毕。当前的视线没有什么效率，也过于简单。在后面将重新组织渲染部分的代码，以免在每一帧都要等待
 	FlushCommandQueue();
 }
